17496a177c9d1d0eee715b605c7e630c801ff691
yishaiy <yishaiy@users.noreply.github.com>
2024-07-26 12:47:40 +0300

    make placeholders easly changable at consts.js file

--- src/searcher.js
+++ src/searcher.js
@@ -30,8 +30,8 @@ function performSearch(event) {
   });
 }
 
-// Function to build search URL by replacing <<TEXT>> in queryTemplate, and inserting full query into engineUrl.
+// Function to build search URL by replacing TEMPLATE_KEYWORD in queryTemplate, and inserting full query into engineUrl.
 function buildSearchUrl(engineUrl, queryTemplate, searchText) {
-  var query = queryTemplate.replace(/<<TEXT>>/g, searchText);
-  return engineUrl.replace(/<<QUERY>>/g, encodeURIComponent(query));
+  var query = queryTemplate.replace(TEMPLATE_KEYWORD, searchText);
+  return engineUrl.replace(QUERY_PLACEHOLDER, encodeURIComponent(query));
 }
